5e5601744cd01af6c9ceceb76737900a779c8165
Stanley Akachukwu <stanley.c.akachukwu@gmail.com>
2022-04-28 12:08:11 +0100

    first commit

---
+++ IdentityServer/Controllers/AdminController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IdentityServer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AdminController : ControllerBase
+    {
+        [HttpGet("GetAdminUsers")]
+        public IActionResult GetAdminUsers()
+        {
+            var users = TestUsers.Users;
+            return Ok(users);
+        }
+    }
+}
---
+++ IdentityServer/Controllers/UsersController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IdentityServer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        [HttpGet("GetUsers")]
+        public IActionResult GetUsers()
+        {
+            var users = TestUsers.Users;
+            return Ok(users);
+        }
+    }
+}
